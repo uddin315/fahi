a4e7870e7b268303c9e80c86b99cfeb581576754
uddin315 <uddinu@outlook.com>
2026-03-24 14:39:23 +0100

    Change credits from Joy-Ahmed to Fahim

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "leave",
  eventType: ["log:unsubscribe"],
  version: "0.0.1",
- credits: "Joy-Ahmed",
+ credits: "Fahim",
  description: "Listen events"
 };
 
@@ -15,9 +15,9 @@ module.exports.run = async({ event, api, Threads, Users }) => {
  if (type == "self-separation") {
 	api.addUserToGroup(event.logMessageData.leftParticipantFbId, event.threadID, (error, info) => {
 	 if (error) {
-		api.sendMessage(`কিরে😂 ${name} তোর এতো বড়ো সাহস😈 আমি জয়
+		api.sendMessage(`কিরে😂 ${name} তোর এতো বড়ো সাহস😈 আমি ফাহিম
  বট থাকতে লিভ নেস😂 :( `, event.threadID)
-	 } else api.sendMessage(`কিরে😈 ${name} কোথায় পালাস আমি জয় বট থাকতে পালাতে পারবি না🤣😂`, event.threadID);
+	 } else api.sendMessage(`কিরে😈 ${name} কোথায় পালাস আমি ফাহিম বট থাকতে পালাতে পারবি না🤣😂`, event.threadID);
 	})
  }
 														}
